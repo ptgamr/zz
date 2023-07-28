f766305f9769294bc43c60cf9368b4794799bd3a
Anh Trinh <anh.trinhtrung@gmail.com>
2023-07-28 14:57:18 +1200

    video texture

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,28 @@
 import "./style.css";
 import * as THREE from "three";
 import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import {
+  CSS2DObject,
+  CSS2DRenderer,
+} from "three/examples/jsm/renderers/CSS2DRenderer.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { gsap } from "gsap";
 
 let renderer: THREE.WebGLRenderer;
+let labelRenderer: CSS2DRenderer;
 let scene: THREE.Scene;
 let camera: THREE.PerspectiveCamera;
 let loader: GLTFLoader;
 let controls: OrbitControls;
 let groundMesh: THREE.Mesh;
+let video: HTMLVideoElement;
+let videoTexture: THREE.VideoTexture;
 
 let jet: GLTF | null = null;
 
 const JET_SCALE = 1;
-const CAMERA_Z_ORIGIN = 200;
+const CAMERA_Z_ORIGIN = 1500;
+const CAMERA_Y_ORIGIN = 300;
 
 function init() {
   renderer = new THREE.WebGLRenderer();
@@ -22,6 +30,7 @@ function init() {
   document.body.appendChild(renderer.domElement);
 
   scene = new THREE.Scene();
+  // scene.background = new THREE.Color(0xffffff);
   // Add an AxesHelper to the scene
   const axesHelper = new THREE.AxesHelper(1000); // The parameter determines the size of the helper
   scene.add(axesHelper);
@@ -30,21 +39,22 @@ function init() {
   // createJet();
   createLight();
   createGround();
+  createText();
 
   controls.update();
 }
 
 function createCamera() {
   camera = new THREE.PerspectiveCamera(
-    75,
+    125,
     window.innerWidth / window.innerHeight,
     0.1,
     1000,
   );
   controls = new OrbitControls(camera, renderer.domElement);
   camera.position.z = CAMERA_Z_ORIGIN;
-  camera.position.y = 100;
-  camera.position.x = 0;
+  camera.position.y = 300;
+  camera.position.x = 300;
   camera.updateProjectionMatrix();
 }
 
@@ -61,15 +71,13 @@ function createLight() {
 }
 
 function createGround() {
-  const groundGeo = new THREE.PlaneGeometry(1000, 1000, 200, 200);
+  const groundGeo = new THREE.PlaneGeometry(3000, 3000, 300, 300);
   const disMap = new THREE.TextureLoader().load("/textures/Heightmap.png");
-  const disScale = 100;
+  const disScale = CAMERA_Y_ORIGIN + 50;
 
-  // const groundGeo = new THREE.PlaneGeometry(1000, 1000, 200, 200);
-  // const disMap = new THREE.TextureLoader().load(
-  //   "/textures/mt-taranaki.png",
-  // );
-  // const disScale = 200
+  // const groundGeo = new THREE.PlaneGeometry(3000, 3000, 200, 200);
+  // const disMap = new THREE.TextureLoader().load("/textures/mt-taranaki.png");
+  // const disScale = CAMERA_Y_ORIGIN + 1000;
 
   disMap.wrapS = disMap.wrapT = THREE.RepeatWrapping;
   disMap.repeat.set(1, 1);
@@ -86,10 +94,55 @@ function createGround() {
   groundMesh.position.y = -0.5;
 }
 
+function createText() {
+  labelRenderer = new CSS2DRenderer();
+  labelRenderer.setSize(window.innerWidth, window.innerHeight);
+  labelRenderer.domElement.style.position = "absolute";
+  labelRenderer.domElement.style.top = "0px";
+  labelRenderer.domElement.style.pointerEvents = "none";
+  document.body.appendChild(labelRenderer.domElement);
+
+  const p = document.createElement("p");
+  p.textContent = "Hello World!";
+  p.style.fontSize = "100px";
+  p.style.color = "red";
+  const layer = new CSS2DObject(p);
+  scene.add(layer);
+  layer.position.set(0, 0, 500);
+
+  // Create a video element
+  video = document.createElement("video");
+  video.style.width = "1280px";
+  video.style.width = "720px";
+  // Get access to the webcam
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch(function (err) {
+      console.log("An error occurred: " + err);
+    });
+
+  // Create a VideoTexture from the video
+  let videoTexture = new THREE.VideoTexture(video);
+  videoTexture.colorSpace = THREE.SRGBColorSpace;
+
+  // Create a mesh with the video texture
+  const geometry = new THREE.PlaneGeometry(1280, 720);
+  const material = new THREE.MeshBasicMaterial({ map: videoTexture });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.z = CAMERA_Z_ORIGIN - 200;
+  mesh.lookAt(camera.position);
+  scene.add(mesh);
+}
+
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
+  labelRenderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 window.addEventListener("resize", onWindowResize, false);
@@ -138,10 +191,7 @@ function createJet() {
 function animate() {
   requestAnimationFrame(animate);
 
-  console.log(camera.position.z);
-
-  // camera.position.z += 0.1;
-  // camera.updateProjectionMatrix();
+  // console.log(camera.position.z);
 
   if (jet) {
     let x = jet.scene.position.x;
@@ -151,9 +201,15 @@ function animate() {
     console.log(jet.scene.position);
   }
 
+  // Update the video texture each frame
+  if (video.readyState === video.HAVE_ENOUGH_DATA && videoTexture) {
+    videoTexture.needsUpdate = true;
+  }
+
   // groundMesh.rotation.x += 0.01;
 
   renderer.render(scene, camera);
+  labelRenderer.render(scene, camera);
 }
 
 init();
